327306841069853a1a687ae591f0c46ce2844944
Jim Wang <jim.wang@spray.com>
2022-08-05 16:33:39 -0500

    use rabbit mq

--- GettingStarted/Consumers/GettingStartedConsumerDefinition.cs
+++ GettingStarted/Consumers/GettingStartedConsumerDefinition.cs
@@ -1,13 +1,13 @@
-namespace Company.Consumers
-{
-    using MassTransit;
+//namespace Company.Consumers
+//{
+//    using MassTransit;
 
-    public class GettingStartedConsumerDefinition :
-        ConsumerDefinition<GettingStartedConsumer>
-    {
-        protected override void ConfigureConsumer(IReceiveEndpointConfigurator endpointConfigurator, IConsumerConfigurator<GettingStartedConsumer> consumerConfigurator)
-        {
-            endpointConfigurator.UseMessageRetry(r => r.Intervals(500, 1000));
-        }
-    }
-}
+//    public class GettingStartedConsumerDefinition :
+//        ConsumerDefinition<GettingStartedConsumer>
+//    {
+//        protected override void ConfigureConsumer(IReceiveEndpointConfigurator endpointConfigurator, IConsumerConfigurator<GettingStartedConsumer> consumerConfigurator)
+//        {
+//            endpointConfigurator.UseMessageRetry(r => r.Intervals(500, 1000));
+//        }
+//    }
+//}
--- GettingStarted/Program.cs
+++ GettingStarted/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using MassTransit;
 using Microsoft.Extensions.DependencyInjection;
+using Company.Consumers;
+using Contracts;
 
 namespace GettingStarted
 {
@@ -20,24 +22,41 @@ namespace GettingStarted
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
-                    services.AddMassTransit(x =>
+                    services.AddMassTransit(bus =>
                     {
-                        x.SetKebabCaseEndpointNameFormatter();
+                        bus.SetKebabCaseEndpointNameFormatter();
 
-                        // By default, sagas are in-memory, but should be changed to a durable
-                        // saga repository.
-                        x.SetInMemorySagaRepositoryProvider();
+                        //// By default, sagas are in-memory, but should be changed to a durable
+                        //// saga repository.
+                        //x.SetInMemorySagaRepositoryProvider();
 
-                        var entryAssembly = Assembly.GetEntryAssembly();
+                        //var entryAssembly = Assembly.GetEntryAssembly();
 
-                        x.AddConsumers(entryAssembly);
-                        x.AddSagaStateMachines(entryAssembly);
-                        x.AddSagas(entryAssembly);
-                        x.AddActivities(entryAssembly);
+                        //x.AddConsumers(entryAssembly);
+                        //x.AddSagaStateMachines(entryAssembly);
+                        //x.AddSagas(entryAssembly);
+                        //x.AddActivities(entryAssembly);
 
-                        x.UsingInMemory((context, cfg) =>
+                        bus.AddConsumer<GettingStartedConsumer>();
+
+                        bus.UsingRabbitMq((context, config) =>
                         {
-                            cfg.ConfigureEndpoints(context);
+                            config.Host("localhost", "/", rabbit =>
+                            {
+                                rabbit.Username("guest");
+                                rabbit.Password("guest");
+                            });
+                            config.Message<HelloMessage>(cfg =>
+                            {
+                                cfg.SetEntityName("weberp.demo");
+                            });
+                            config.ReceiveEndpoint("weberp.demo.test", cfgEndpoint =>
+                            {
+                                cfgEndpoint.ConfigureConsumeTopology = false;
+                                cfgEndpoint.Bind<HelloMessage>();
+                                cfgEndpoint.ConfigureConsumer<GettingStartedConsumer>(context);
+                            });
+
                         });
                     });
                     services.AddHostedService<Worker>();
